c0b6012557f79fb902a68d6a4fae9d54be1f01a4
siddarthreddygsr <siddarthreddy4923@gmail.com>
2022-07-06 23:15:35 +0530

    Project Setup

---
+++ Spot-Menu/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Spot-Menu
+//
+//  Created by Siddarth Reddy on 06/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
