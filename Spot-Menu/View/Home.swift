61af002819b78d300e90c8f63c83f2dc1275aba0
siddarthreddygsr <siddarthreddy4923@gmail.com>
2022-07-07 01:01:24 +0530

    designed base App UI

--- Spot-Menu/View/Home.swift
+++ Spot-Menu/View/Home.swift
@@ -10,9 +10,58 @@ import SwiftUI
 struct Home: View {
     var body: some View {
         VStack{
-            
+            HStack{
+//                Spacer()
+                Button(action: {
+                    NSApplication.shared.terminate(nil)
+                }, label: {
+                    Image(systemName: "rectangle.portrait.and.arrow.right")
+                        .resizable()
+                        .frame(width: 20,height:20)
+                })
+                .padding(.leading,250)
+                .padding(.top,10)
+                
+            }
+            HStack{
+                Image("track")
+                .resizable()
+                .frame(width: 90,height: 90)
+                .padding(10)
+                VStack{
+                    Text("Let Me Down Slowly")
+                        .font(.system(size: 20))
+                    Text("Alec Benjamin")
+                }
+                .frame(width: 190)
+                
+                
+            }
+            HStack{
+                Image(systemName: "backward.end.fill")
+                    .resizable()
+                    .frame(width:20,height: 20)
+                    .padding(10)
+                Image(systemName: "gobackward.10")
+                    .resizable()
+                    .frame(width:27,height: 27)
+                    .padding(5)
+                Image(systemName: "play.fill")
+                    .resizable()
+                    .frame(width:30,height: 30)
+                    .padding(5)
+                Image(systemName: "goforward.10")
+                    .resizable()
+                    .frame(width:27,height: 27)
+                    .padding(5)
+                Image(systemName: "forward.end.fill")
+                    .resizable()
+                    .frame(width:20,height: 20)
+                    .padding(10)
+            }
+            Spacer()
         }
-        .frame(width: 320, height: 450)
+        .frame(width: 320, height: 250)
     }
 }
 
