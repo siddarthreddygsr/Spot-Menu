63c469ba4fa1dddf246c37460d866b66cb1a7ad8
siddarthreddygsr <siddarthreddy4923@gmail.com>
2022-07-08 04:18:18 +0530

    connected all buttons

--- Spot-Menu/View/Home.swift
+++ Spot-Menu/View/Home.swift
@@ -35,7 +35,6 @@ struct Home: View {
                 VStack{
                     Text("\(updaterViewModel.trackname)")
                         .font(.system(size: 20))
-                    
                     Text("\(updaterViewModel.artistname)")
                 }
                 .frame(width: 190)
@@ -43,33 +42,69 @@ struct Home: View {
                 
             }
             HStack{
-                Image(systemName: "backward.end.fill")
-                    .resizable()
-                    .frame(width:20,height: 20)
-                    .padding(10)
-                Image(systemName: "gobackward.10")
-                    .resizable()
-                    .frame(width:27,height: 27)
-                    .padding(5)
+                Button(action: {
+                    let response = SpotifyHelpers.tellSpotify(command: "previous track")
+                    print(response)
+                }, label: {
+                    Image(systemName: "backward.end.fill")
+                        .resizable()
+                        .frame(width:20,height: 20)
+                        .padding(10)
+                })
+                .buttonStyle(PlainButtonStyle())
+                
+                Button(action: {
+                    let response = SpotifyHelpers.tellSpotify(command: "set player position to player position - 10 ")
+                    print(response)
+                }, label: {
+                    Image(systemName: "gobackward.10")
+                        .resizable()
+                        .frame(width:27,height: 27)
+                        .padding(5)
+                })
+                .buttonStyle(PlainButtonStyle())
+                
                 Button(action: {
                     let response = SpotifyHelpers.tellSpotify(command: "playpause")
                     print(response)
                 }, label: {
-                    Image(systemName: "play.fill")
-                                        .resizable()
-                                        .frame(width:30,height: 30)
-                                        .padding(5)
+                    if updaterViewModel.playerstate == "playing" {
+                        Image(systemName: "pause.fill")
+                            .resizable()
+                            .frame(width:30,height: 30)
+                            .padding(5)
+                    } else {
+                        Image(systemName: "play.fill")
+                            .resizable()
+                            .frame(width:30,height: 30)
+                            .padding(5)
+                    }
                 })
                 .buttonStyle(PlainButtonStyle())
 
-                Image(systemName: "goforward.10")
-                    .resizable()
-                    .frame(width:27,height: 27)
-                    .padding(5)
-                Image(systemName: "forward.end.fill")
-                    .resizable()
-                    .frame(width:20,height: 20)
-                    .padding(10)
+                Button(action: {
+                    let response = SpotifyHelpers.tellSpotify(command: "set player position to player position + 10 ")
+                    print(response)
+                }, label: {
+                    Image(systemName: "goforward.10")
+                        .resizable()
+                        .frame(width:27,height: 27)
+                        .padding(5)
+                })
+                .buttonStyle(PlainButtonStyle())
+                
+                
+                Button(action: {
+                    let response = SpotifyHelpers.tellSpotify(command: "next track")
+                    print(response)
+                }, label: {
+                    Image(systemName: "forward.end.fill")
+                        .resizable()
+                        .frame(width:20,height: 20)
+                        .padding(10)
+                })
+                .buttonStyle(PlainButtonStyle())
+               
             }
             Spacer()
         }
@@ -107,7 +142,7 @@ struct Home_Previews: PreviewProvider {
 class UpdaterViewModel: ObservableObject {
     @Published var trackname:String = "ERROR"
     @Published var artistname:String = "ERROR"
-
+    @Published var playerstate:String = "paused"
     var timer: Timer?
     init() {
         timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { _ in
@@ -120,5 +155,6 @@ class UpdaterViewModel: ObservableObject {
     func refresh() {
         trackname = SpotifyHelpers.tellSpotify(command: "get name of current track")
         artistname = SpotifyHelpers.tellSpotify(command: "get artist of current track")
+        playerstate = SpotifyHelpers.tellSpotify(command: "get player state")
     }
 }
